b3d1b1f4a2e87243a3e61aa406f9df8a12a012bd
zhenyulei <zhenyulei@jd.com>
2020-05-20 14:52:57 +0800

    update

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-var cacheName = 'helloPwa1'; //缓存的名称                
-self.addEventListener('install', event => { //安装
-  //确保 Service Worker 不会在 waitUntil() 里代码执行完毕之前安装完成。
-  event.waitUntil( 
-    
-  );
-});
